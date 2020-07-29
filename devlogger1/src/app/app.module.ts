900916a23a647470eff5689b3338f5560d213223
appsius <ilyas.ustunkaya234434@gmail.com>
2020-07-29 12:06:46 +0300

    Observables & Component Communication

--- devlogger1/src/app/app.module.ts
+++ devlogger1/src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LogFormComponent } from './components/log-form/log-form.component';
-import { LogsComponent } from './components/logs/logs.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LogFormComponent } from "./components/log-form/log-form.component";
+import { LogsComponent } from "./components/logs/logs.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LogFormComponent,
-    LogsComponent
+    LogsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
